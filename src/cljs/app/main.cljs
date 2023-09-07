b0cbde4408793bf744e30c6f8a0b1b8a9e1346df
Jacob Doran <j.doran.admin@gmail.com>
2023-09-07 19:26:50 +0800

    add main page

---
+++ src/cljs/app/main.cljs
@@ -0,0 +1,114 @@
+(ns app.main
+  (:require-macros
+   [lib.helix-wrapper :as lh])
+  (:require
+   [components.tag :as tag]
+   [helix.core :refer [$ <>]]
+   [helix.dom :as d]
+   [util.date :as date]
+   [util.metadata :as metadata]))
+
+(defn post [{:keys [slug date title summary tags]}]
+  (d/li
+   {:key slug
+    :class-name "py-12"}
+   ($ "article"
+      (d/div
+       {:class-name "space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline"}
+       (d/dl
+        (d/dt
+         {:class-name "sr-only"}
+         "Published on")
+        (d/dd
+         {:class-name "text-base font-medium leading-6 text-gray-500 dark:text-gray-400"}
+         (d/time
+          {:date-time date}
+          (date/format-date date (metadata/site :locale)))))
+       (d/div
+        {:class-name "space-y-5 xl:col-span-3"}
+        (d/div
+         {:class-name "space-y-6"}
+         (d/div
+          (d/h2
+           {:class-name "text-2xl font-bold leading-8 tracking-tight"}
+           (d/a
+            {:href (str "/blog/" slug)
+             :class-name "text-gray-900 dark:text-gray-100"}
+            title))
+          (d/div
+           {:class-name "flex flex-wrap"}
+           (map (fn [t] ($ tag/tag {:text t})) tags)))
+         (d/div
+          {:class-name "prose max-w-none text-gray-500 dark:text-gray-400"}
+          summary)
+         (d/div
+          {:class-name "text-base font-medium leading-6"}
+          (d/a
+           {:href (str "/blog/" slug)
+            :class-name "text-primary-500 hover:text-primary-600"
+            :aria-label (str "Read " title)}
+           "Read more →"))))))))
+
+(lh/defnc home []
+  (let [MAX_DISPLAY 5
+        posts [{:slug "1"
+                :date "2023-09-07"
+                :summary "sure thing bro"
+                :tags ["a" "b" "c"]
+                :title "1"}
+               {:slug "2"
+                :date "2023-09-07"
+                :summary "sure thing bro"
+                :tags ["a" "b" "c"]
+                :title "2"}
+               {:slug "3"
+                :date "2023-09-07"
+                :summary "sure thing bro"
+                :tags ["a" "b" "c"]
+                :title "3"}
+               {:slug "4"
+                :date "2023-09-07"
+                :summary "sure thing bro"
+                :tags ["a" "b" "c"]
+                :title "4"}
+               {:slug "5"
+                :date "2023-09-07"
+                :summary "sure thing bro"
+                :tags ["a" "b" "c"]
+                :title "5"}
+               {:slug "6"
+                :date "2023-09-07"
+                :summary "sure thing bro"
+                :tags ["a" "b" "c"]
+                :title "6"}
+               {:slug "7"
+                :date "2023-09-07"
+                :summary "sure thing bro"
+                :tags ["a" "b" "c"]
+                :title "7"}]]
+    (<>
+     (d/div
+      {:class-name "divide-y divide-gray-200 dark:divide-gray-700"}
+      (d/div
+       {:class-name "space-y-2 pb-8 pt-6 md:space-y-5"}
+       (d/h1
+        {:class-name "text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14"}
+        "Latest")
+       (d/p
+        {:class-name "text-lg leading-7 text-gray-500 dark:text-gray-400"}
+        (metadata/site :description)))
+      (d/ul
+       {:class-name "divide-y divide-gray-200 dark:divide-gray-700"}
+       (if (= 0 (count posts))
+         "No posts found."
+         (->> posts
+              (take MAX_DISPLAY)
+              (map post)))))
+     (if (> (count posts) MAX_DISPLAY)
+       (d/div
+        {:class-name "flex justify-end text-base font-medium leading-6"}
+        (d/a
+         {:href "/blog/"
+          :class-name "text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+          :aria-label "All posts"}
+         "All posts →"))))))
